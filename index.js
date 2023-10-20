f28810dfd523f373569434e7b3f6f392d5faa513
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2023-10-20 19:56:14 +0600

    added edited file of index.js

--- index.js
+++ index.js
@@ -23,11 +23,6 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-
-
-
-
-
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
